74632ce3f60af3e4e045f5864057a2917654be7a
Daniel Farrell <djfarrell@failingfastest.com>
2022-05-08 16:14:29 +1000

    harvey_ghidra: add json command parsing
    Add json command parsing for when complex data needs to be used.

--- harvey_ghidra/src/main/java/harvey/HarveyCmds.java
+++ harvey_ghidra/src/main/java/harvey/HarveyCmds.java
@@ -8,30 +8,79 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
 
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+
 public class HarveyCmds {
 
 	public HarveyPlugin plugin;
-	public Map<String, Function<List<String>, String>> commands;
+	public Map<String, Function<Map<String, String>, String>> commands;
 
 	public HarveyCmds(HarveyPlugin _plugin) {
 		plugin = _plugin;
-		commands = new HashMap<String, Function<List<String>, String>>();
+		commands = new HashMap<String, Function<Map<String, String>, String>>();
 		commands.put("echo", this::echo);
+		commands.put("jsoncmd", this::jsonCmd);
 	}
 
-	public String echo(List<String> input) {
+	public String echo(Map<String, String> input) {
 		String output = "ECHO: ";
 		int index = 0;
-		for (String s : input) {
-			if (index > 0) {
-				output = output + s;
-				if (index < input.size() - 1) {
-					output = output + " ";
-				}
+		for (var o : input.entrySet()) {
+			output = output + o.getKey() + ":" + o.getValue();
+			if (index < input.size() - 1) {
+				output = output + " ";
 			}
 			index++;
 		}
 		return output;
+	}
+
+	public String jsonCmd(Map<String, String> input) {
+		JSONParser jp = new JSONParser();
+		JSONObject jo;
+		String ret;
+		Function<Map<String, String>, String> func = null;
+		String json = input.get("json");
+
+		if (json == null) {
+			return "error: no json provided";
+		}
+
+		try {
+			jo = (JSONObject)jp.parse(input.get("json"));
+			if (jo == null) {
+				return "error: bad parse";
+			}
+			if (jo.containsKey("cmd")) {
+				func = commands.get(jo.get("cmd"));
+			} else {
+				return "error: no command provided";
+			}
+			if (func == null) {
+				return "error: command does not exist";
+			}
+			
+		} catch (ParseException pe) {
+			return pe.toString();
+		} catch (Exception e) {
+			return e.toString();
+		}
+
+		Map<String, String> m = new HashMap<String, String>();
+		for (Object k : jo.keySet()) {
+			try {
+				if (!((String)k).equals("cmd")) {
+					m.put((String)k, jo.get(k).toString());
+				}
+			} catch (Exception e) {
+				return "error: bad json value";
+			}
+		}
+
+		return func.apply(m);
 	}
 
 	public String doCommand(String line) {
@@ -41,15 +90,29 @@ public class HarveyCmds {
 			return "error: no command provided";
 		}
 
-		Function<List<String>, String> func = commands.get(parts[0]);
+		if (parts[0].equals("jsoncmd")) {
+			Map<String, String> jsonInput = new HashMap<String, String>();
+			jsonInput.put("json", line.substring(parts[0].length() + 1));
+
+			return jsonCmd(jsonInput);
+		}
+
+		Function<Map<String, String>, String> func = commands.get(parts[0]);
 		if (func == null) {
 			return "error: command does not exist";
 		}
 		
-		List<String> filteredParts = new ArrayList<String>();
+		line = line.substring(parts[0].length() + 1);
+		parts = line.split(" ");
+
+		Map<String, String> filteredParts = new HashMap<String, String>();
 		for (String s : parts) {
-			if (s != " ") {
-				filteredParts.add(s);
+			String[] part_split = s.split("=");
+
+			if (part_split.length > 1) {
+				filteredParts.put(part_split[0], part_split[1]);
+			} else {
+				filteredParts.put(part_split[0], "");
 			}
 		}
 
--- harvey_ghidra/src/test/java/harvey/tests/HarveyIOTests.java
+++ harvey_ghidra/src/test/java/harvey/tests/HarveyIOTests.java
@@ -22,13 +22,50 @@ class HarveyIOTests {
 		h.textCmd.setText("echo one");
 		h.actionPerformed(null);
 
-		assertEquals("\nECHO: one", h.textArea.getText());
+		assertEquals("\nECHO: one:", h.textArea.getText());
 		assertEquals("", h.textCmd.getText());
 
-		h.textCmd.setText("echo two");
+		h.textCmd.setText("echo two=something");
 		h.actionPerformed(null);
 
-		assertEquals("\nECHO: one\nECHO: two", h.textArea.getText());
+		assertEquals("\nECHO: one:\nECHO: two:something", h.textArea.getText());
 		assertEquals("", h.textCmd.getText());
 	}
+
+	@Test
+	public void testBadCommand() {
+		HarveyCmds c = new HarveyCmds(null);
+		HarveyIO h = new HarveyIO(null, c);
+
+		h.textCmd.setText("not a command");
+		h.actionPerformed(null);
+
+		assertEquals("\nerror: command does not exist", h.textArea.getText());
+		assertEquals("", h.textCmd.getText());
+	}
+
+	@Test
+	public void testCommandNotProvided() {
+		HarveyCmds c = new HarveyCmds(null);
+		HarveyIO h = new HarveyIO(null, c);
+
+		h.textCmd.setText("jsoncmd {\"not_cmd\": \"no_command\"}");
+		h.actionPerformed(null);
+
+		assertEquals("\nerror: no command provided", h.textArea.getText());
+		assertEquals("", h.textCmd.getText());
+	}
+
+	@Test
+	public void testEchoJson() {
+		HarveyCmds c = new HarveyCmds(null);
+		HarveyIO h = new HarveyIO(null, c);
+
+		h.textCmd.setText("jsoncmd {\"cmd\": \"echo\", \"arg1\": 1}");
+		h.actionPerformed(null);
+
+		assertEquals("\nECHO: arg1:1", h.textArea.getText());
+		assertEquals("", h.textCmd.getText());
+	}
+
 }
